c59321aba5cd33f634a7eefe504e77a1684a2681
Chris Lemke <spcbrn1@gmail.com>
2018-01-22 17:44:11 -0700

    initial commit, custom boilerplate loaded

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <main className="App">
+        <h1>Time to build something...</h1>
+      </main>
+    )
+  }
+};
+
+export default App;
