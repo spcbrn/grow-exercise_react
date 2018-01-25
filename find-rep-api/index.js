1dd36c2e42aa163ab62ae6d13ff4ca5afc1242ea
Chris Lemke <spcbrn1@gmail.com>
2018-01-25 15:03:12 -0700

    set up static server

--- find-rep-api/index.js
+++ find-rep-api/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const request = require('request');
 const cors    = require('cors');
+const path    = require('path');
 const app     = express();
 
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, './../dist_build')))
 
 app.get('/representatives/:state',
   findRepresentativesByState,
